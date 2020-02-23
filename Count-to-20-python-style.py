e0cb9ba9a4ffe69874a4e77ac3b9d9de7a453283
ecdegroot <ecdegroot@gmail.com>
2020-02-23 20:47:26 +0100

    Create Count-to-20-python-style.py

---
+++ Count-to-20-python-style.py
@@ -0,0 +1,73 @@
+# 1
+print (1)
+
+# 2
+number = 2
+print (number)
+
+# 3
+number = number + 1
+print(number)
+
+# 4,5,6
+while number <= 5:
+  number = number + 1
+  print (number)
+
+# 7
+number = int(14 / 2)
+print(number)
+
+# 8
+number = 2*4
+print(number)
+
+# 9
+number = len('123456789')
+print(number)
+
+# 10
+number = int(10*1)
+print(number)
+
+# 11
+number = int('1' + '1')
+print(number)
+
+# 12
+list = [1,2,3,4,5,6,7,8,9,10,11,12]
+number = list [11]
+print(number)
+
+# 13
+dictionary = {1:13 ,2:14}
+number =  dictionary[1]
+print(number)
+
+# 14
+dictionary = {1:13 ,2:14}
+number =  dictionary[2]
+print(number)
+
+# 15,16
+for aantal in range ( 14, 16 ):
+  number =  number + 1
+  print(number)
+
+# 17
+number_string =  '17'
+number = int (number_string)
+print(number)
+
+# 18
+PI = 3.14
+number = round(PI * 5.6)
+print(number)
+
+# 19
+number = int('19')
+print(number)
+
+# 20
+number = 20000/1000
+print (int(number))
